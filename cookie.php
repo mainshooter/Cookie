777e1a1e9091a7db3ce0b5600b1d3aad6a8c51a5
Peter 4A5A <498883@edu.rocmn.nl>
2017-09-14 09:11:45 +0200

    Added the if statement

--- cookie.php
+++ cookie.php
@@ -1,5 +1,17 @@
 <?php
 
+  if (ISSET($_COOKIE['accepted-terms'])) {
+    if ($_COOKIE['accepted-terms'] === true) {
+      // Client accpted the terms
+    }
 
+    else {
+      // Client deny the terms
+    }
+  }
+
+  else {
+    // Client is here for the first time
+  }
 
 ?>
