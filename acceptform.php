647f5327ca2d9f2540c20d116a333771568c82b6
Peter 4A5A <498883@edu.rocmn.nl>
2017-09-14 09:47:20 +0200

    Added a niet toestaan input

--- acceptform.php
+++ acceptform.php
@@ -2,4 +2,5 @@
   <h3>Onze website gebruikt cookies!</h3>
   <p>U dient akkoord te gaan met de voorwaarden.</p>
   <input type="submit" name="cookieAcceptation" value="toestaan">
+  <input type="submit" name="cookieAcceptation" value="Niet toestaan">
 </form>
