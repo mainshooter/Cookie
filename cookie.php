6a615ab95588e0ad46c1f1bf69ed062844b280ca
Peter 4A5A <498883@edu.rocmn.nl>
2017-09-14 09:19:16 +0200

    Added al the things that were needed

--- cookie.php
+++ cookie.php
@@ -1,17 +1,32 @@
 <?php
 
   if (ISSET($_COOKIE['accepted-terms'])) {
-    if ($_COOKIE['accepted-terms'] === true) {
+    if ($_COOKIE['accepted-terms'] == true) {
       // Client accpted the terms
+      echo "<h1>U heeft onze voorwaarden geaccepteerd!</h1>";
     }
 
     else {
       // Client deny the terms
+      displayCookieAcceptForm();
     }
   }
 
   else {
     // Client is here for the first time
+    displayCookieAcceptForm();
+  }
+
+  if (ISSET($_POST['cookieAcceptation'])) {
+    if ($_POST['cookieAcceptation'] === 'toestaan') {
+      // They allow to have cookies
+      setcookie('accepted-terms', true, time() + 10);
+      header("Refresh:0");
+    }
+  }
+
+  function displayCookieAcceptForm() {
+    include 'acceptform.php';
   }
 
 ?>
