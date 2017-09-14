318e65c931415c325bed33b9cf0ad48eb3381da2
Peter 4A5A <498883@edu.rocmn.nl>
2017-09-14 09:40:01 +0200

    Wrote the cookie class

--- index.php
+++ index.php
@@ -1,33 +1,33 @@
 <?php
 
-  if (ISSET($_COOKIE['accepted-terms'])) {
-    if ($_COOKIE['accepted-terms'] == true) {
-      // Client accpted the terms
-      echo "<h1>U heeft onze voorwaarden geaccepteerd!</h1>";
-    }
-
-    else {
-      // Client deny the terms
-      displayCookieAcceptForm();
-    }
-  }
-
-  else {
-    // Client is here for the first time, so the need to accept our coockie terms
-    displayCookieAcceptForm();
-  }
-
-  if (ISSET($_POST['cookieAcceptation'])) {
-    if ($_POST['cookieAcceptation'] === 'toestaan') {
-      // They allow to have cookies
-      setcookie('accepted-terms', true, time() + 10);
-      header("Refresh:0");
-    }
-  }
-
-  function displayCookieAcceptForm() {
-    include 'acceptform.php';
-  }
+  // if (ISSET($_COOKIE['accepted-terms'])) {
+  //   if ($_COOKIE['accepted-terms'] == true) {
+  //     // Client accpted the terms
+  //     echo "<h1>U heeft onze voorwaarden geaccepteerd!</h1>";
+  //   }
+  //
+  //   else {
+  //     // Client deny the terms
+  //     displayCookieAcceptForm();
+  //   }
+  // }
+  //
+  // else {
+  //   // Client is here for the first time, so the need to accept our coockie terms
+  //   displayCookieAcceptForm();
+  // }
+  //
+  // if (ISSET($_POST['cookieAcceptation'])) {
+  //   if ($_POST['cookieAcceptation'] === 'toestaan') {
+  //     // They allow to have cookies
+  //     setcookie('accepted-terms', true, time() + 10);
+  //     header("Refresh:0");
+  //   }
+  // }
+  //
+  // function displayCookieAcceptForm() {
+  //   include 'acceptform.php';
+  // }
 
   class Cookie {
     public $cookieName;
@@ -46,7 +46,7 @@
 
       else {
         // We can set cookies
-        echo "<h3>We can set cookies!</h3>";
+
       }
     }
 
@@ -55,7 +55,7 @@
       return($form);
     }
 
-    private function checkIfCookiesAreAllowed() {
+    public function checkIfCookiesAreAllowed() {
       if (ISSET($_COOKIE['accepted-terms'])) {
         // The have the cookie
         if ($_COOKIE['accepted-terms'] == true) {
@@ -75,6 +75,32 @@
       return($this->cookieAcceptedTerms);
     }
 
+    public function saveClientCookieTerms($cookieTermsFromClient) {
+      if ($cookieTermsFromClient) {
+        // If they want cookies
+        $this->cookieAcceptedTerms = true;
+        setcookie('accepted-terms', true, time() + 10);
+      }
+
+      else {
+        // The don't want cookies
+        $this->cookieAcceptedTerms = false;
+      }
+
+      header("Refresh:0");
+    }
+
+  }
+
+   $Cookie = new Cookie();
+
+  if (ISSET($_POST['cookieAcceptation'])) {
+    $Cookie->saveClientCookieTerms($_POST['cookieAcceptation']);
+  }
+
+  if ($Cookie->checkIfCookiesAreAllowed()) {
+    // Cookies are allowed
+    echo "<h1>Cookies zijn toegestaan!</h1>";
   }
 
 ?>
