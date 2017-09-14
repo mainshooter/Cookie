fae9301a710b7e2969d55b57f6f41884336b0aab
Peter 4A5A <498883@edu.rocmn.nl>
2017-09-14 09:16:37 +0200

    Added form method

--- acceptform.php
+++ acceptform.php
@@ -1,4 +1,4 @@
-<form>
+<form method="post">
   <h3>Onze website gebruikt cookies!</h3>
   <p>U dient akkoord te gaan met de voorwaarden.</p>
   <input type="submit" name="cookieAcceptation" value="toestaan">
